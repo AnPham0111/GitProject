645ddc666d666248001ae79d75048c3197a62840
PhamAn <ffqpda0111@gmail.com>
2022-03-27 01:42:06 +0700

    an

---
+++ baithuchanhjs.js
@@ -0,0 +1,26 @@
+let i = 10;
+let  f = 20.5;
+let b = true;
+let s = 'Ha Noi';
+document.write('i=' +i);
+document.write('</br>');
+document.write('f=' +f);
+document.write('</br>');
+document.write('b=' +b);
+document.write('</br>');
+document.write('s=' +s);
+document.write('</br>');
+let width = 20;
+let height =10;
+let area = width * height
+document.write('Area=' + area);
+document.write('</br>');
+x = prompt("Nhap x:");
+y = prompt("Nhap y:");
+if (x%y == 0){
+    alert('x chia het cho y')
+}
+else {
+    alert('x khong chia het cho y')
+}
+
---
+++ th.js
@@ -0,0 +1,5 @@
+var demo=document.getElementById("b3");
+demo.addEventListener("click",b3        );
+function b3(){
+    alert('hi')
+}
